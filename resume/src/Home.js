7d225b95aa2f1f011b0c116223af5f2a80a9c223
Nazanin <n.taghavi168@gmail.com>
2021-08-17 20:01:15 +0430

    Filter

--- resume/src/Home.js
+++ resume/src/Home.js
@@ -5,15 +5,21 @@ const Home = () => {
     // let name = 'Nazanin';
     const [skills, setskills] = useState(
         [
-            {title: 'HTML', body: 'lorem ipsum ...', class: 'Front-end', id: 1},
-            {title: 'CSS', body: 'lorem ipsum ...', class: 'Front-end', id: 2},
-            {title: 'JS', body: 'lorem ipsum ...',class: 'Front-end', id: 3},
-            {title: 'React.JS', body: 'lorem ipsum ...',class: 'Front-end', id: 4}
+            {title: 'HTML', body: 'lorem ipsum ...', class: 'Web Development', id: 1},
+            {title: 'CSS', body: 'lorem ipsum ...', class: 'Web Development', id: 2},
+            {title: 'JS', body: 'lorem ipsum ...',class: 'Web Development', id: 3},
+            {title: 'React.JS', body: 'lorem ipsum ...',class: 'Web Development', id: 4},
+            {title: 'Python', body: 'lorem ipsum ...', class: 'AI', id: 5},
+            {title: 'Machine Learning', body: 'lorem ipsum ...', class: 'AI', id: 6},
+            {title: 'Deep Learning', body: 'lorem ipsum ...',class: 'AI', id:7},
+            {title: 'Data Mining', body: 'lorem ipsum ...',class: 'AI', id:8},
+            {title: 'SEO', body: 'lorem ipsum ...',class: 'Web Development', id: 9}
         ]
     );
     return (  
         <div className="home">
-         <SkillList skills={skills} title="My Skills"/>   
+         <SkillList skills={skills.filter((skill)=>skill.class==='Web Development')} title="My Web Skills"/> 
+         <SkillList skills={skills.filter((skill)=>skill.class==='AI')} title="My AI Skills"/>   
         </div>
     );
 }
