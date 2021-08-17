ddf9bb3b1f9ee0b27d577e28dae5476d1f8b77d7
Nazanin <n.taghavi168@gmail.com>
2021-08-17 17:47:24 +0430

    added title

--- resume/src/App.js
+++ resume/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 
 function App() {
+
+  const title = 'Welcome to my Resume!';
+  const fname = 'Nazanin', lname = 'Taghavi';
+  const jobs = ['Front-end Developer' , 'AI Engineer'];
   return (
     <div className="App">
      <div className="content">
-       <h1>App Components</h1>
+       <h1>{ title }</h1>
+       <h3>{fname } {lname}</h3>
+       <p>{jobs[0]} & {jobs[1]}</p>
+
      </div>
     </div>
   );
