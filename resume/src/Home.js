dee25d2be59d042a8aef5783bf7f89cf34632884
Nazanin <n.taghavi168@gmail.com>
2021-08-17 19:06:53 +0430

    use state hook

--- resume/src/Home.js
+++ resume/src/Home.js
@@ -1,10 +1,15 @@
+import { useState } from "react";
+
 const Home = () => {
+    // let name = 'Nazanin';
+    const [name, setName] = useState('Nazanin');
     const clickHandle = () =>{
-        console.log('Hello');
+      setName('Taghavi');
     }
     return (  
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name}</p>
             <button onClick={clickHandle}>Click me!</button>
         </div>
     );
